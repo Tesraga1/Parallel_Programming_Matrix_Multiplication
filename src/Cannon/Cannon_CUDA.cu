609a238247d5ea2628d3caeef9bdf43a0666154c
Tesraga1 <jbproffesional6@gmail.com>
2026-04-19 17:26:24 -0400

    src file setup

---
+++ src/Cannon/Cannon_CUDA.cu
@@ -0,0 +1,15 @@
+//
+// Created by blancj4 on 4/18/2026.
+//
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdbool.h>
+#include<math.h>
+#include<cuda.h>
+#include<cuda_runtime.h>
+#include <string.h>
+
+extern "C"{
+    void runCuda();
+}
